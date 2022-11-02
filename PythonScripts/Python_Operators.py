87fb22bff414a3d7595f1f6679bb0d370b0f0266
Riley Anderson <10803452@uvu.edu>
2022-11-01 22:51:55 -0600

    did some Python learning

---
+++ PythonScripts/Python_Operators.py
@@ -0,0 +1,31 @@
+# More refrence w3schools.com
+
+name = "Gillion"
+print(name)
+print(type(name))
+
+name = 4.5
+print(name)
+print(type(name))
+
+# string ("str")    :    "Gillion", any text
+#                        'Gillion', 'Riley'
+#                        """Gillion""", """Rilty"""
+
+# integer (int)     :    Whole numbers
+
+# float  (float)    :    Whole and decemal numbers
+
+# boolean (bool)    :    1 or 0, True or False
+
+# collections
+#   - list          :   ["Gillion", 27, True, .344] - you can change add diffrent varriables, and use duplicats
+
+#   - tuple         :   ("Gillion", "Riley", "Lucy") - cant change the array and with ()
+
+#   - set           :   {"Gillion", "Riley", "Lucy"} - there is not order and unchange able, not duplicats
+#
+#   - dictionary    :   pet = {"type" : "cat", "color" : "orange", "personalit" : "annolying"} - no duplicats
+#     (dict)            print (pet["color"])
+#
+#   -  NoneType (None) : None
