d990defb64b169268254ef64c355bf9ce076abda
Riley Anderson <10803452@uvu.edu>
2023-09-18 16:01:33 -0600

    Made a broken FK script and forgot to push it

--- PythonScripts/NurbCurveColorChange.py
+++ PythonScripts/NurbCurveColorChange.py
@@ -4,11 +4,11 @@ import maya.cmds as cmds
 def rig_color(color):
     sels = cmds.ls(sl=True)
 
-    shape = cmds.listRelatives (sels, shapes = True)
+    shape = cmds.listRelatives(sels, shapes=True)
 
     for selection in shape:
             cmds.setAttr(selection + '.overrideEnabled', 1)
             cmds.setAttr(selection + '.overrideColor', color)
 
 
-rig_color(13)
+rig_color(6)
