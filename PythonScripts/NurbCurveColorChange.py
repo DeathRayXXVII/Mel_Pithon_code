40095bb90347039e15589f01395623ac2a7c4bfa
RileyA <scout1553@gmail.com>
2024-02-16 10:20:37 -0700

    updated my nurbCurve script so it has a UI to pick the color

--- PythonScripts/NurbCurveColorChange.py
+++ PythonScripts/NurbCurveColorChange.py
@@ -1,14 +1,31 @@
-import maya.cmds as cmds
+#import maya.cmds as cmds
 
 
-def rig_color(color):
-    sels = cmds.ls(sl=True)
+#def rig_color(color):
+    #sels = cmds.ls(sl=True)
 
-    shape = cmds.listRelatives(sels, shapes=True)
+   # shape = cmds.listRelatives(sels, shapes=True)
 
-    for selection in shape:
-            cmds.setAttr(selection + '.overrideEnabled', 1)
-            cmds.setAttr(selection + '.overrideColor', color)
+    #for selection in shape:
+            #cmds.setAttr(selection + '.overrideEnabled', 1)
+            #cmds.setAttr(selection + '.overrideColor', color)
 
 
-rig_color(6)
+#rig_color(6)
+def create_ui():
+    if cmds.window("myWindow", exists=True):
+        cmds.deleteUI("myWindow", window=True)
+
+    my_window = cmds.window("myWindow", title="Color Change UI", widthHeight=(200, 100))
+    cmds.columnLayout(adjustableColumn=True)
+
+    color_field = cmds.textField(text='Color Index')
+
+    def change_color(*args):
+        color_index = cmds.textField(color_field, query=True, text=True)
+        rig_color(int(color_index))
+
+    cmds.button(label="Change Color", command=change_color)
+    cmds.showWindow(my_window)
+
+create_ui()
