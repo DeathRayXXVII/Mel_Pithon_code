c62599b779bc34bceb0d947498b8ef5203d622a4
Riley Anderson <10803452@uvu.edu>
2024-02-16 22:34:34 -0700

    Fix a small error

--- PythonScripts/NurbCurveColorChange.py
+++ PythonScripts/NurbCurveColorChange.py
@@ -1,17 +1,16 @@
-#import maya.cmds as cmds
+import maya.cmds as cmds
 
 
-#def rig_color(color):
-    #sels = cmds.ls(sl=True)
+def rig_color(color):
+    sels = cmds.ls(sl=True)
 
-   # shape = cmds.listRelatives(sels, shapes=True)
+    shape = cmds.listRelatives(sels, shapes=True)
 
-    #for selection in shape:
-            #cmds.setAttr(selection + '.overrideEnabled', 1)
-            #cmds.setAttr(selection + '.overrideColor', color)
+    for selection in shape:
+        cmds.setAttr(selection + '.overrideEnabled', 1)
+        cmds.setAttr(selection + '.overrideColor', color)
 
 
-#rig_color(6)
 def create_ui():
     if cmds.window("myWindow", exists=True):
         cmds.deleteUI("myWindow", window=True)
@@ -27,5 +26,6 @@ def create_ui():
 
     cmds.button(label="Change Color", command=change_color)
     cmds.showWindow(my_window)
+
 
 create_ui()
